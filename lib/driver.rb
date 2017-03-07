d3fad053cd29fe3da8d0fe3f924a817d217a2308
Janice Lichtman <jalichtman@gmail.com>
2017-03-06 21:47:07 -0800

    added a before() bloct to driver_spec

--- lib/driver.rb
+++ lib/driver.rb
@@ -11,5 +11,22 @@ module RideShare
       @vin = args[:vin]
       raise ArgumentError.new("Invalid vin") if vin.length != 17
     end
+
+
+    # def self.all
+    #    all_drivers =[]
+    #    CSV.open("support/drivers.csv").each do |line|
+    #      all_drivers << self.new({id:line[0], name:line[1], vin:line[2]}
+    #    end
+    #    return all_drivers
+    #
+    #  end
+
+
+# CSV.foreach("../support/trips.csv", {:headers => true}) do |row|
+
+
+
+
   end
 end
--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -9,37 +9,43 @@ require_relative '../lib/driver'
 #Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 
+
 describe "RideShare::Driver" do
+
+  before do
+    @new_driver_hash = {id:'42', name:'Granville Mertz', vin:'1B9TPKC24YPL290Y4'}
+    @new_driver = RideShare::Driver.new(@new_driver_hash)
+  end
 
   describe "RideShare::Driver#initialize" do
 
     it "Creates a new instance of Driver class" do
-      new_driver_hash = {id:'42', name:'Granville Mertz', vin:'1B9TPKC24YPL290Y4'}
-      new_driver = RideShare::Driver.new(new_driver_hash)
-      new_driver.must_be_instance_of RideShare::Driver
+      # new_driver_hash = {id:'42', name:'Granville Mertz', vin:'1B9TPKC24YPL290Y4'}
+      # new_driver = RideShare::Driver.new(new_driver_hash)
+      @new_driver.must_be_instance_of RideShare::Driver
     end
 
     it "Takes a argument hash and assigns an id, name, and vin" do
-      new_driver_hash = {id:'42', name:'Granville Mertz', vin:'1B9TPKC24YPL290Y4'}
-      new_driver = RideShare::Driver.new(new_driver_hash)
+      # new_driver_hash = {id:'42', name:'Granville Mertz', vin:'1B9TPKC24YPL290Y4'}
+      # new_driver = RideShare::Driver.new(new_driver_hash)
 
-      new_driver.must_respond_to :id
-      new_driver.id.must_equal new_driver_hash[:id]
+      @new_driver.must_respond_to :id
+      @new_driver.id.must_equal @new_driver_hash[:id]
 
-      new_driver.must_respond_to :name
-      new_driver.name.must_equal new_driver_hash[:name]
+      @new_driver.must_respond_to :name
+      @new_driver.name.must_equal @new_driver_hash[:name]
 
-      new_driver.must_respond_to :vin
-      new_driver.vin.must_equal new_driver_hash[:vin]
+      @new_driver.must_respond_to :vin
+      @new_driver.vin.must_equal @new_driver_hash[:vin]
     end
-
-    it "Raises an ArgumentError when created with an invalid vin (ie. vin of wrong length) " do
-      bad_vin_hash = {vin:'1B9TPKC24YPL290Y4-9999'}
-
-      proc {
-        new_driver = RideShare::Driver.new(bad_vin_hash)
-      }.must_raise ArgumentError
-    end
+    #
+    # it "Raises an ArgumentError when created with an invalid vin (ie. vin of wrong length) " do
+    #   bad_vin_hash = {vin:'1B9TPKC24YPL290Y4-9999'}
+    #
+    #   proc {
+    #     new_driver = RideShare::Driver.new(bad_vin_hash)
+    #   }.must_raise ArgumentError
+    # end
 
   end
 end
