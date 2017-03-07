3bd078ab50f0f322e673c0b5dcbb43878a4e0fbf
Janice Lichtman <jalichtman@gmail.com>
2017-03-06 16:40:32 -0800

    created and tested drivier initialization method, including test if vin length

--- lib/driver.rb
+++ lib/driver.rb
@@ -3,6 +3,13 @@ module RideShare
 
   class Driver
 
-  
+    attr_reader :id, :name, :vin
+
+    def initialize(args)
+      @id = args[:id]
+      @name = args[:name]
+      @vin = args[:vin]
+      raise ArgumentError.new("Invalid vin") if vin.length != 17
+    end
   end
 end
