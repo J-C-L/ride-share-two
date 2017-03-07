e95310219e47958a5e9bc48808acbb8439c69277
Janice Lichtman <jalichtman@gmail.com>
2017-03-06 22:29:37 -0800

    Wrote and tested RideShare::Driver.all method, including readin in csv file

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,3 +1,4 @@
+require 'csv'
 
 module RideShare
 
@@ -12,21 +13,18 @@ module RideShare
       raise ArgumentError.new("Invalid vin") if vin.length != 17
     end
 
-
-    # def self.all
-    #    all_drivers =[]
-    #    CSV.open("support/drivers.csv").each do |line|
-    #      all_drivers << self.new({id:line[0], name:line[1], vin:line[2]}
-    #    end
-    #    return all_drivers
-    #
-    #  end
-
-
-# CSV.foreach("../support/trips.csv", {:headers => true}) do |row|
-
-
+    def self.all
+      all_drivers =[]
+      CSV.foreach("support/drivers.csv", {:headers => true}) do |line|
+        all_drivers << self.new({id:line[0].to_i, name:line[1], vin:line[2]})
+      end
+      return all_drivers
+    end
 
 
   end
 end
+
+#puts RideShare::Driver.all[5].name
+
+#puts CSV.read("support/drivers.csv").length
--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -6,6 +6,8 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/driver'
 
+require 'csv'
+
 #Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 
@@ -38,14 +40,40 @@ describe "RideShare::Driver" do
       @new_driver.must_respond_to :vin
       @new_driver.vin.must_equal @new_driver_hash[:vin]
     end
-    #
-    # it "Raises an ArgumentError when created with an invalid vin (ie. vin of wrong length) " do
-    #   bad_vin_hash = {vin:'1B9TPKC24YPL290Y4-9999'}
-    #
-    #   proc {
-    #     new_driver = RideShare::Driver.new(bad_vin_hash)
-    #   }.must_raise ArgumentError
-    # end
+
+    it "Raises an ArgumentError when created with an invalid vin (ie. vin of wrong length) " do
+      bad_vin_hash = {vin:'1B9TPKC24YPL290Y4-9999'}
+
+      proc {
+        new_driver = RideShare::Driver.new(bad_vin_hash)
+      }.must_raise ArgumentError
+    end
 
   end
+
+
+
+  describe "RideShare::Driver.all" do
+
+    it "Returns an array of Driver instances" do
+      RideShare::Driver.all.must_be_instance_of Array, "Not an array."
+      RideShare::Driver.all.each do |driver|
+        driver.must_be_instance_of   RideShare::Driver, "Not an instance of Driver class."
+      end
+    end
+
+    it "Returns an array with the correct number of drivers" do
+      number_of_drivers = CSV.read("support/drivers.csv").length - 1
+      RideShare::Driver.all.length.must_equal number_of_drivers, "Wrong number of drivers"
+    end
+
+    it "gives correct values for the ID, NAME, and VIN of the last
+    driver in the CSV file" do
+    RideShare::Driver.all.last.id.must_equal 100, "ID of last driver is incorrect."
+    RideShare::Driver.all.last.name.must_equal 'Minnie Dach', "NAME of last driver is incorrect."
+    RideShare::Driver.all.last.vin.must_equal 'XF9Z0ST7X18WD41HT', "VIN of last driver is incorrect."
+    end
+  end
+
+
 end
