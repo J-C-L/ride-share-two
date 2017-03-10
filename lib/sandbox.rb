3457ac47b79561b6b706b7d865fb7ed2e946efd4
Janice Lichtman <jalichtman@gmail.com>
2017-03-10 00:43:21 -0800

    fixed problem with writing to the csv. solevd in sandbox. next to branch and implement solution

---
+++ lib/sandbox.rb
@@ -0,0 +1,20 @@
+require 'csv'
+
+# For optionals, adding cost and duration to the trips.csv file.
+# Assigning random durations of 0 to 120 minutes, and using $0.50 per minute as trip cost.
+trips = CSV.read('support/trips.csv')
+trips[0] += ['cost', 'duration']
+
+(1...trips.length).each do |count|
+  duration = rand(120)
+  cost = 0.50 * duration
+  trips[count][5] = cost
+  trips[count][6] = duration
+end
+
+
+CSV.open('support/trips_optionals.csv', 'w') do |file|
+  trips.each do |line|
+    file << line
+  end
+end
