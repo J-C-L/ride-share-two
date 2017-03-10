f56aa5fc9aa52322b3c03b66098c480f642aae09
Janice Lichtman <jalichtman@gmail.com>
2017-03-10 00:29:29 -0800

    messed up trips.csv file. will revert to before branch creation and start over

--- lib/sandbox.rb
+++ lib/sandbox.rb
@@ -4,19 +4,27 @@ require 'csv'
 #CSV.parse("support/trips_optionals.csv") { |row1| puts row1.inspect }
 
 
-trips = CSV.read('support/trips_optionals.csv')
-trips[0] += ['cost', 'duration']
-(1...trips.length).each do |count|
-  duration = rand(120)
-  cost = 0.50 * duration
-  trips[count] += [cost,duration]
+trips = CSV.read('support/trips.csv')
+trips.each do |line|
+  (1..8).each do |num|
+    line.delete_at(0-num)
+  end
 end
 
-CSV.open('support/trips_optionals.csv', 'w') do |file|
+CSV.open('support/trips.csv', 'w') do |file|
   trips.each do |line|
     file << line
   end
 end
+
+# trips[0] += ['cost', 'duration']
+# (1...trips.length).each do |count|
+#   duration = rand(120)
+#   cost = 0.50 * duration
+#   trips[count] += [cost,duration]
+# end
+#
+
 
 # print trips[0]
 # puts
--- lib/trip.rb
+++ lib/trip.rb
@@ -1,10 +1,27 @@
 require 'csv'
 
+# For optionals, adding cost and duration to the trips.csv file.
+# Assigning random durations of 0 to 120 minutes, and using $0.50 per minute as trip cost.
+trips = CSV.read('support/trips.csv')
+trips[0] += ['cost', 'duration']
+(1...trips.length).each do |count|
+  duration = rand(120)
+  cost = 0.50 * duration
+  trips[count] += [cost,duration]
+end
+
+CSV.open('support/trips.csv', 'w') do |file|
+  trips.each do |line|
+    file << line
+  end
+end
+
+
 module RideShare
 
   class Trip
 
-    attr_reader :trip_id, :driver_id, :rider_id, :date, :rating
+    attr_reader :trip_id, :driver_id, :rider_id, :date, :rating, :cost, :duration
 
     def initialize(args)
       @trip_id = args[:trip_id]
@@ -12,6 +29,8 @@ module RideShare
       @rider_id = args[:rider_id]
       @date = args[:date]
       @rating = args[:rating]
+      @cost = args[:cost]
+      @duration = args[:duration]
 
       raise ArgumentError.new("Rating must be a number from 1 to 5.") if ((@rating.class != Integer) && (@rating.class != Float))
       raise Invalid_Rating_Error.new("Rating must be from 1 to 5.") if !(1 <= @rating.to_f && @rating.to_f <=5)
@@ -20,7 +39,7 @@ module RideShare
     def self.all
       all_trips =[]
       CSV.foreach("support/trips.csv", {:headers => true}) do |line|
-        all_trips << self.new( {trip_id:line[0].to_i, driver_id:line[1].to_i, rider_id:line[2].to_i, date:line[3], rating:line[4].to_i} )
+        all_trips << self.new( {trip_id:line[0].to_i, driver_id:line[1].to_i, rider_id:line[2].to_i, date:line[3], rating:line[4].to_i, cost:line[5].to_i, duration:line[6].to_i} )
       end
       return all_trips
     end
@@ -49,3 +68,13 @@ module RideShare
 
   end
 end
+
+
+puts RideShare::Trip.all[0].trip_id
+puts
+puts RideShare::Trip.all[1].date
+puts
+puts RideShare::Trip.all[2].cost
+puts
+puts RideShare::Trip.all[-1].duration
+puts
